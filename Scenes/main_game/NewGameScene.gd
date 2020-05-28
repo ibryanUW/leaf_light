36ae9bcbf9a47b646f2cd88865f57e1649003d06
Ian <ibryan@uw.edu>
2020-05-27 21:11:12 -0700

    Global Positioning, Path-Finding Updates
    Finally got the global positioning to show itself for important objects on the screen.

--- Scenes/main_game/NewGameScene.gd
+++ Scenes/main_game/NewGameScene.gd
@@ -8,17 +8,16 @@ func _ready():
 
 func _input(event):
 	if(event.is_action_pressed('click')):
-		if(sun_gen == null):
-			print("click")
-			new_game_screen.add_child(sun_gen)
-		else:
-			print("was not null, click")
+		if(sun_gen != null):
 			if($"Game_Start_Info_Label" != null):
 				$"Game_Start_Info_Label".queue_free()
 			sun_gen.free()
-			print(player_collision_info())
 			sun_gen = SunlightTileGenerator.new()
 			new_game_screen.add_child(sun_gen)
+			print(get_sun_gen_coords(), sun_gen.get_child(1).get_global_position())
+			player_collision_info()
+		else:
+			print("was null")
 
 func player_collision_info():
 	print($"PlayerCharacter/Player".get_rect())
@@ -26,3 +25,7 @@ func player_collision_info():
 	print($"PlayerCharacter/Player".get_texture().get_size())
 	print($"PlayerCharacter/PlayerCollisionBox".get_shape().get_extents().round())
 	print($"PlayerCharacter".get_position())
+#	print($"PlayerCharacter".get_position().distance_to())
+
+func get_sun_gen_coords():
+	return sun_gen.get_parent()
